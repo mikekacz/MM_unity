3cc6f0f397fd9b92a25f2f683c6842db77680cbf
Tomasz Olszewski <ullihtch@yahoo.com>
2015-03-27 22:35:10 +0100

    Added 11th Scoreboard row if last score is not in the 10 highest.

--- Assets/Scripts/PlayerScoreList.cs
+++ Assets/Scripts/PlayerScoreList.cs
@@ -26,7 +26,7 @@ public class PlayerScoreList : MonoBehaviour {
 	// Draw the Scoreboard
 	void DrawScoreboard () {
 		int rowNumber = 10;	// Number of Scoreboard rows
-		int counter = 0;	// Scoreboard row counter
+		int counter = 1;	// Scoreboard row counter
 
 		// User list is empty -> nothing to draw
 		if (playerList == null)
@@ -43,25 +43,26 @@ public class PlayerScoreList : MonoBehaviour {
 		if (playerList.Count < rowNumber)
 			rowNumber = playerList.Count;
 
-		// Draw and fill the rows
-		for (; counter < rowNumber; ) {
-			// Current player
-			GameUser user = playerList[counter];
+		// Go through the user list
+		foreach (GameUser user in playerList) {
+			// draw 10 rows and an 11th if the last score is not in those 10
+			if (counter <= rowNumber || user.Equals (lastPlayer)) {
+				// Create row and add it to the vertical layout group
+				GameObject go = (GameObject) Instantiate (playerScoreEntryPrefab);
+				go.transform.SetParent (this.transform, false);
+
+				// Find matching user and highlight his entry green
+				if (user.Equals (lastPlayer))
+					go.transform.GetComponent<Image>().color = new Color(0.59f, 1.0f, 0.55f, 0.59f);
+
+				// Fill the values of the row
+				go.transform.Find("Rank").GetComponent<Text>().text = counter.ToString();
+				go.transform.Find("Name").GetComponent<Text>().text = user.Name;
+				go.transform.Find("Surname").GetComponent<Text>().text = user.Surname;
+				go.transform.Find("Score").GetComponent<Text>().text = user.score.ToString();
+			}
+
 			counter++;
-
-			// Create row and add it to the vertical layout group
-			GameObject go = (GameObject) Instantiate (playerScoreEntryPrefab);
-			go.transform.SetParent (this.transform, false);
-
-			// Find matching user and highlight his entry green
-			if (user.Equals (lastPlayer))
-				go.transform.GetComponent<Image>().color = new Color(0.59f, 1.0f, 0.55f, 0.59f);
-
-			// Fill the values of the row
-			go.transform.Find("Rank").GetComponent<Text>().text = counter.ToString();
-			go.transform.Find("Name").GetComponent<Text>().text = user.Name;
-			go.transform.Find("Surname").GetComponent<Text>().text = user.Surname;
-			go.transform.Find("Score").GetComponent<Text>().text = user.score.ToString();
 		}
 	}
 }
