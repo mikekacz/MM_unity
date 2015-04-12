02e2d43db502ae39c45761b3b798e7e68087e247
PLKaczmareM <PLKaczmareM@LDZ9967N.g02.fujitsu.local>
2015-04-12 18:39:38 +0200

    Feature: android - switching screens with swipe
    Signed-off-by: PLKaczmareM <PLKaczmareM@LDZ9967N.g02.fujitsu.local>

--- Assets/Scripts/ScoreboardScript.cs
+++ Assets/Scripts/ScoreboardScript.cs
@@ -3,16 +3,52 @@ using System.Collections;
 
 public class ScoreboardScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	private Vector2 touchPosPrev;
+	private bool couldBeSwipe;
+	private int errorMargin = 10;
+	private int minSwipeDist =10;
+
+	void switchScreen (){
+		this.gameObject.SetActive(false);
+		GameObject.Find("/Canvas").transform.GetChild(0).gameObject.SetActive(true);
 	}
-	
+		
 	// Update is called once per frame
 	void Update () {
 		// F1 Key pressed
 		if (Input.GetKeyDown(KeyCode.F1)) {
-			this.gameObject.SetActive(false);
-			GameObject.Find("/Canvas").transform.GetChild(0).gameObject.SetActive(true);
+			switchScreen ();
 		}
+		// http://forum.unity3d.com/threads/swipe-help-please.48601/#post-323387
+		else if ((Input.touchCount == 1)){
+			//get only touch info
+			var touch = Input.GetTouch(0);
+			
+			switch (touch.phase){
+			case TouchPhase.Began :
+				couldBeSwipe = true;
+				touchPosPrev = touch.position;
+				break;
+			case TouchPhase.Moved :
+				break;
+				
+			case TouchPhase.Stationary:
+				couldBeSwipe = false;
+				break;
+			case TouchPhase.Ended:
+				var swipeVector = touch.position - touchPosPrev;
+				if (couldBeSwipe && (swipeVector.magnitude>minSwipeDist) && ((swipeVector.x / swipeVector.y > 4) || (swipeVector.x / swipeVector.y < -4) )){
+					switchScreen();
+				}
+				break;
+			case TouchPhase.Canceled:
+				couldBeSwipe =false;
+				break;
+			}
+			
+		}
+		
 	}
+
+
 }
